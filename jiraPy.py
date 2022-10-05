9adb9f7ffe58dd93f84a527313fb397ef5d27f21
Oscar Lindholm <olindhol@redhat.com>
2022-10-05 16:39:40 +0200

    Added functionality to both read/inspect issues as well as commenting on them

--- jiraPy.py
+++ jiraPy.py
@@ -1,9 +1,9 @@
 from jira.client import JIRA, JIRAError
-import logging
 import argparse
 import configparser
 from prettytable import PrettyTable
 import sys
+import textwrap
 
 # Loading configuration
 config = configparser.ConfigParser()
@@ -15,6 +15,12 @@ support_project = config.get("variables", "support_project")
 # Aliases - need cleanup
 Done = config.get("aliases", "Done")
 Closed = config.get("aliases", "Closed")
+# Text formatting
+BOLD = '\033[1m'
+OKGREEN = '\033[92m'
+OKCYAN = '\033[96m'
+ENDC = '\033[0m'
+FAIL = '\033[91m'
 
 # Creating parser
 parser = argparse.ArgumentParser(prog='jiraPy',
@@ -22,7 +28,7 @@ parser = argparse.ArgumentParser(prog='jiraPy',
                                  description='Browse Jira through the terminal',
                                  formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=80))
 
-# Arguments
+# Arguments (Add Inspect issue - to read the description of the task etc)
 parser.add_argument('-l', '--list', 
                     action='store_true',
                     help='List current issues')
@@ -30,11 +36,8 @@ parser.add_argument('-l', '--list',
 parser.add_argument('-s', '--support', 
                     action='store_true',
                     help='List assigned support issues')
-parser.add_argument('--status', 
-                    action='store_true',
-                    help='Print status for %s' % base_project)
 parser.add_argument('-c', '--comment', 
-                    nargs=2,
+                    nargs='*',
                     help='Comment selected issue')
 parser.add_argument('-m', '--move', 
                     nargs=2,
@@ -57,29 +60,22 @@ wflow_dict = {'Backlog': 11,
              'Rejected': 81
              }
 
-
 # Defines a function for connecting to Jira
-def connect_jira(log, server, token):
+def connect_jira(server, token):
     '''
     Connect to JIRA. Return None on error
     '''
     try:
-        log.info("Connected to: %s" % server)
         jira_options = {'server': server}
         jira = JIRA(options=jira_options, token_auth=(token))
         return jira
     except Exception as e:
-        log.error("Failed to connect to JIRA: %s" % e)
+        print("Could not connect to the server")
         return None
 
-# create logger
-log = logging.getLogger("jiraPy")
-#logging.basicConfig(level=logging.DEBUG)
-
-
 def get_issues(project_name):
-    jira = connect_jira(log, server, token)
-    issue_table = PrettyTable(['Issue', 'Status', 'Description', 'Creation Date'])
+    jira = connect_jira(server, token)
+    issue_table = PrettyTable(['Issue', 'Type', 'Status', 'Description', 'Creation Date'])
     issues = jira.search_issues('assignee = currentUser() AND project = %s' %(project_name))
     for issue in issues:
         if Done in str(issue.fields.status):
@@ -87,46 +83,55 @@ def get_issues(project_name):
         elif Closed in str(issue.fields.status):
             pass
         else:
-            issue_table.add_row([issue, issue.fields.status, issue.fields.summary, issue.fields.created[:10]])
+            issue_table.add_row([issue, issue.fields.issuetype, issue.fields.status, issue.fields.summary, issue.fields.created[:10]])
     if issue_table.rows:
         print(issue_table)
     else:
         print(f"Nothing assigned in project {project_name}")
 
-def get_status():
-    jira = connect_jira(log, server, token)
-    status_table = PrettyTable(['Issue', 'Status', 'Description', 'Creation Date', 'Assignee'])
-    issues = jira.search_issues('project = %s' %(base_project))
-    for issue in issues:
-        status_table.add_row([issue, issue.fields.status, issue.fields.summary, issue.fields.created[:10], issue.fields.assignee])
-    print(status_table)
-
-def add_comment(j_issue, comment):
-    jira = connect_jira(log, server, token)
-    try:
-        jira.add_comment(j_issue, comment)
-        print(f"The issue \033[1m {j_issue} \033[0m was commented with: \nComment: {comment}")
-    except JIRAError:
-        print(f"Could not add comment to {j_issue}")
-
-        
+def add_comment(j_issue, comment=""):
+    jira = connect_jira(server, token)
+    if not comment:
+        comment_count = 1
+        try:
+            issue = jira.issue(j_issue)
+            comments = issue.fields.comment.comments
+            if issue.fields.description is not None:
+                print(f"{OKGREEN}Description:{ENDC}\n\n",textwrap.fill(issue.fields.description, 79))
+            for comment in comments:
+                print("=" *79)
+                print(f"{comment_count}. {OKCYAN}Comment:{ENDC}")
+                print(f"{textwrap.fill(comment.body, 79)}\n\n Author: {comment.author.displayName}\n Date: {comment.created[:10]}\n")
+                comment_count = comment_count + 1
+        except JIRAError:
+            print(f"{FAIL}ERROR:{ENDC} Could not read {j_issue}, The issue was not found")
+    else:
+        try:
+            jira.add_comment(j_issue, comment)
+            print(f"The issue {BOLD} {j_issue} {ENDC} was commented with: \n{OKCYAN}Comment:{ENDC} {comment}")
+        except JIRAError:
+            print(f"{FAIL}ERROR:{ENDC} Could not add comment to {j_issue}")
 
 def transition_issue(issue, w_flow):
-    jira = connect_jira(log, server, token)
+    jira = connect_jira(server, token)
     print("Moving issue: %s to: %s" % (issue, args.move[1]))
     try:
         jira.transition_issue(issue, w_flow)
     except JIRAError:
         print(f"Issue: {args.move[0]} does not exist, try again")
 
+# Add argument to add assignee
+# Note that custom fields are specific for the environment etc. Inspect the code on your browser to see
+# What values are relevant.
 def create_issue(summary, issue_type="Feil", description="Adding later"):
-    jira = connect_jira(log, server, token)
+    jira = connect_jira(server, token)
     issue_dict = {
     'project': {'key': base_project},
     'summary': summary,
     'description': description,
     'issuetype': {'name': issue_type},
-    'assignee': {'name': jira.current_user()}
+    'assignee': {'name': jira.current_user()},
+    'customfield_17400': {'value': 'OpenShift'}
 }
     try:
         jira.create_issue(fields=issue_dict)
@@ -140,10 +145,13 @@ def main():
         get_issues(base_project)
     if args.support:
         get_issues(support_project)
-    if args.status:
-        get_status()
     if args.comment:
-        add_comment(args.comment[0], args.comment[1])
+        if len(args.comment) == 2:
+            add_comment(args.comment[0], args.comment[1])
+        elif len(args.comment) == 1:
+            add_comment(args.comment[0])
+        else:
+            print("Contains too many arguments got: \n",', '.join(args.comment))
     if args.move:
         if args.move[1] in wflow_dict:
             transition_issue(args.move[0], wflow_dict[args.move[1]])
